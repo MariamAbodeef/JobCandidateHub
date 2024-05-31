203c08e0fd2d7f3a8f22124ed182d430b85a83bf
mabodeef <mabodeef@arabdt.com>
2024-05-31 13:09:55 +0300

    Create Job Candidate Unit tests

---
+++ JobCandidateHub.API.Tests/Controllers.Tests/JobCandidateControllerTests.cs
@@ -0,0 +1,134 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using JobCandidateHub.API.Controllers;
+using JobCandidateHub.Application.Abstraction;
+using JobCandidateHub.Domain.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace JobCandidateHub.API.Tests.Controllers.Tests
+{
+    public class JobCandidateControllerTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IJobCandidateService> _mockService;
+        private readonly JobCandidateController _controller;
+
+        public JobCandidateControllerTests()
+        {
+            _fixture = new Fixture();
+            _mockService = new Mock<IJobCandidateService>();
+            _controller = new JobCandidateController(_mockService.Object);
+        }
+
+        [Fact]
+        public async Task GetAllCandidates_ShouldReturnOk()
+        {
+            // Arrange
+
+            var email = _fixture.Create<string>();
+            var candidates = _fixture.Create<List<JobCandidateDto>>();
+            _mockService.Setup(service => service.GetCandidates(email)).ReturnsAsync(candidates);
+
+
+            // Act
+            var result = await _controller.GetAllCandidates(email);
+
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ActionResult<List<JobCandidateDto>>>();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value.Should().BeEquivalentTo(candidates);
+            _mockService.Verify(x => x.GetCandidates(email), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetAllCandidates_WithNullEmail_ShouldReturnOk()
+        {
+            // Arrange
+            string email = null;
+            var candidates = _fixture.Create<List<JobCandidateDto>>();
+            _mockService.Setup(service => service.GetCandidates(email)).ReturnsAsync(candidates);
+
+            // Act
+            var result = await _controller.GetAllCandidates(email);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ActionResult<List<JobCandidateDto>>>();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value.Should().BeEquivalentTo(candidates);
+            _mockService.Verify(x => x.GetCandidates(email), Times.Once());
+
+        }
+        
+        
+        [Fact]
+        public async Task GetAllCandidates_WithEmptyEmail_ShouldReturnOk()
+        {
+            // Arrange
+            string email = String.Empty;
+            var candidates = _fixture.Create<List<JobCandidateDto>>();
+            _mockService.Setup(service => service.GetCandidates(email)).ReturnsAsync(candidates);
+
+            // Act
+            var result = await _controller.GetAllCandidates(email);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ActionResult<List<JobCandidateDto>>>();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value.Should().BeEquivalentTo(candidates);
+            _mockService.Verify(x => x.GetCandidates(email), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetAllCandidates_WithNotExistEmail_ShouldReturnNotFound()
+        {
+            // Arrange
+            string email = _fixture.Create<string>(); ;
+            List<JobCandidateDto> candidates = null;
+            _mockService.Setup(service => service.GetCandidates(email)).ReturnsAsync(candidates);
+
+            // Act
+            var result = await _controller.GetAllCandidates(email);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<NotFoundResult>();
+            _mockService.Verify(x => x.GetCandidates(email), Times.Once());
+
+        }
+        [Fact]
+        public async Task AddOrUpdateCandidate_WithNullInput_ShouldReturnBadRequest()
+        {
+            // Arrange
+            JobCandidateDto candidate = null;
+
+            // Act
+            var result = await _controller.AddOrUpdateCandidate(candidate);
+
+            // Assert
+            result.Result.Should().BeAssignableTo<BadRequestResult>();
+
+        }
+
+        [Fact]
+        public async Task AddOrUpdateCandidate_ShouldReturnOk()
+        {
+            // Arrange
+            var candidate = new JobCandidateDto();
+            _mockService.Setup(service => service.AddOrUpdateCandidate(candidate)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.AddOrUpdateCandidate(candidate);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            result.Result.As<OkObjectResult>().Value.Should().Be(true);
+        }
+    }
+}
---
+++ JobCandidateHub.DataAccessLayer.Tests/Repositories.Tests/JobCandidateRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using JobCandidateHub.DataAccessLayer.Context;
+using JobCandidateHub.DataAccessLayer.Repositories;
+using JobCandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using AutoFixture;
+using System.Data.Common;
+
+namespace JobCandidateHub.DataAccessLayer.Tests.Repositories.Tests
+{
+    public class JobCandidateRepositoryTests
+    {
+        [Fact]
+        public async Task AddCandidateAsync_ShouldAddCandidate()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "RepositoryTestsDB")
+                .Options;
+
+            var mockContext = new ApplicationDbContext(options);
+            var repository = new JobCandidateRepository(mockContext);
+
+            var candidate = new JobCandidate { Email = "tst@me.me" , FirstName="tst", LastName="tst" };
+
+            // Act
+            var result = await repository.AddCandidateAsync(candidate);
+
+            // Assert
+            result.Should().BeTrue();
+            mockContext.JobCandidates.Should().Contain(candidate);
+        }
+
+        [Fact]
+        public void GetAllCandidatesAsQueryable_ShouldReturnFilteredCandidates()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "RepositoryTestsDB")
+                .Options;
+
+            var candidates = new List<JobCandidate>
+            {
+                new JobCandidate { Email = "tst1@me.me", FirstName="tst1", LastName= "tst1" },
+                new JobCandidate { Email = "tst2@me.me", FirstName="tst2", LastName= "tst2"},
+                new JobCandidate { Email = "tst3@me.me", FirstName="tst3", LastName= "tst3"}
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.JobCandidates.AddRange(candidates);
+                context.SaveChanges();
+            }
+
+            var mockContext = new ApplicationDbContext(options);
+            var repository = new JobCandidateRepository(mockContext);
+
+            // Act
+            var result = repository.GetAllCandidatesAsQueryable("tst2@me.me").ToList();
+
+            // Assert
+            result.Should().HaveCount(1);
+            result[0].Email.Should().Be("tst2@me.me");
+        }
+
+        
+
+    }
+
+}
