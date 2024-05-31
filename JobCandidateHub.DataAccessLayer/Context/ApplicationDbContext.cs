f1f3c69aa367c340e59445537ae8a84759ee1eaf
mabodeef <mabodeef@arabdt.com>
2024-05-31 13:08:24 +0300

    Database setup and context

---
+++ JobCandidateHub.DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using JobCandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace JobCandidateHub.DataAccessLayer.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public IDbConnection Connection => Database.GetDbConnection();
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JobCandidate>()
+            .HasKey(c => c.Email);
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<JobCandidate> JobCandidates { get; set; }
+
+    }
+}
---
+++ JobCandidateHub.Domain/Entities/JobCandidate.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHub.Domain.Entities
+{
+    public class JobCandidate
+    {
+
+        [Required(ErrorMessage = "Please enter First name")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Last name")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits")]
+        public string PhoneNumber { get; set; }
+
+        
+        [Required(ErrorMessage = "Please enter Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        public string Email { get; set; }
+
+        public string BestCallTime { get; set; } 
+
+        [Url(ErrorMessage = "Invalid LinkedIn profile URL")]
+        [StringLength(200, ErrorMessage = "LinkedIn profile URL cannot exceed 200 characters")]
+        public string LinkedInProfileUrl { get; set; }
+
+        [Url(ErrorMessage = "Invalid GitHub profile URL")]
+        [StringLength(200, ErrorMessage = "GitHub profile URL cannot exceed 200 characters")]
+        public string GitHubProfileUrl { get; set; }
+
+        public string Comment { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
